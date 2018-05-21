5b9964138f867c5dd4b57cfb09d5eba314cae79d
nihce <nihce@github.com>
2018-05-21 11:20:59 +0200

    indent popravljen

--- Arduino/Koda Miha/test_program/klic_test_fun/klic.ino
+++ Arduino/Koda Miha/test_program/klic_test_fun/klic.ino
@@ -9,57 +9,57 @@ void setup() {
 void loop() {
 
   if(robot.buttonPressed()){
-		delay(1000); //zakasnitev za debouncing
+    delay(1000); //zakasnitev za debouncing
 
-		Serial.println("--- ZAČETEK TESTA BATERIJE ---");
-		Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
+    Serial.println("--- ZAČETEK TESTA BATERIJE ---");
+    Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
     test.battery();
-		Serial.println("--- KONEC TESTA BATERIJE ---");
-		Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
-		delay(2000); //zakasnitev za zagotavljanje stabilnosti delovanja
+    Serial.println("--- KONEC TESTA BATERIJE ---");
+    Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
+    delay(2000); //zakasnitev za zagotavljanje stabilnosti delovanja
 
-		Serial.println("--- ZAČETEK TESTA LEVEGA MOTORJA ---");
-		Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
+    Serial.println("--- ZAČETEK TESTA LEVEGA MOTORJA ---");
+    Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
     test.motor(LEFT);
-		Serial.println("--- KONEC TESTA LEVEGA MOTORJA ---");
-		Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
-		delay(2000); //zakasnitev za zagotavljanje stabilnosti delovanja
+    Serial.println("--- KONEC TESTA LEVEGA MOTORJA ---");
+    Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
+    delay(2000); //zakasnitev za zagotavljanje stabilnosti delovanja
 
-		Serial.println("--- ZAČETEK TESTA DESNEGA MOTORJA ---");
-		Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
+    Serial.println("--- ZAČETEK TESTA DESNEGA MOTORJA ---");
+    Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
     test.motor(RIGHT);
-		Serial.println("--- KONEC TESTA DESNEGA MOTORJA ---");
-		Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
-		delay(2000); //zakasnitev za zagotavljanje stabilnosti delovanja
+    Serial.println("--- KONEC TESTA DESNEGA MOTORJA ---");
+    Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
+    delay(2000); //zakasnitev za zagotavljanje stabilnosti delovanja
 
-		Serial.println("--- ZAČETEK TESTA BRENČAČA ---");
-		Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
+    Serial.println("--- ZAČETEK TESTA BRENČAČA ---");
+    Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
     test.buzzer();
-		Serial.println("--- KONEC TESTA BRENČAČA ---");
-		Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
-		delay(2000); //zakasnitev za zagotavljanje stabilnosti delovanja
+    Serial.println("--- KONEC TESTA BRENČAČA ---");
+    Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
+    delay(2000); //zakasnitev za zagotavljanje stabilnosti delovanja
 
-		Serial.println("--- ZAČETEK TESTA SENZORJEV ZA RAZDALJO ---");
-		Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
+    Serial.println("--- ZAČETEK TESTA SENZORJEV ZA RAZDALJO ---");
+    Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
     test.distance();
-		Serial.println("--- KONEC TESTA SENZORJEV ZA RAZDALJO ---");
-		Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
-		delay(2000); //zakasnitev za zagotavljanje stabilnosti delovanja
+    Serial.println("--- KONEC TESTA SENZORJEV ZA RAZDALJO ---");
+    Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
+    delay(2000); //zakasnitev za zagotavljanje stabilnosti delovanja
 
-		Serial.println("--- ZAČETEK TESTA SENZORJEV ZA SVETLOBO ---");
-		Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
+    Serial.println("--- ZAČETEK TESTA SENZORJEV ZA SVETLOBO ---");
+    Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
     test.line();
-		Serial.println("--- KONEC TESTA SENZORJEV ZA SVETLOBO ---");
-		Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
-		delay(2000); //zakasnitev za zagotavljanje stabilnosti delovanja
+    Serial.println("--- KONEC TESTA SENZORJEV ZA SVETLOBO ---");
+    Serial.println(); //izpis prazne vrstice na serijski monitor za lepši pregled
+    delay(2000); //zakasnitev za zagotavljanje stabilnosti delovanja
 
     //To spodaj ne deluje na 2. verziji robota
-		/*
+    /*
     test.temp();
     test.mag();
     test.accel();
     test.gyro();
-		*/
+    */
   }
 
   delay(10); //zakasnitev za zagotavljanje stabilnosti delovanja
